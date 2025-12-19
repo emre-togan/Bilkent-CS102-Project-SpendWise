ef9e43580f8a7fb6740d7389fcec21e5b1886543
Emirhan Efe Suçeken <efe.suceken@ug.bilkent.edu.tr>
2025-12-19 19:27:02 +0300

    Update user class
    fix the user class

--- Models/User.java
+++ Models/User.java
@@ -1,7 +1,47 @@
 package Models;
 
+import java.security.Timestamp;
+
 public class User {
     
+    private int id;
+    private String userName;
+    private String password;
+    private String eMail;
+    private Timestamp accountCreationTime;
     
+    public User(String userName, String password, String eMail, int id, Timestamp accountCreationTime){
+        this.userName = userName;
+        this.password = password;
+        this.eMail = eMail;
+        this.id = id;
+        this.accountCreationTime = accountCreationTime;
+    }
+
+    public User(String userName, String password, String eMail){
+        this.userName = userName;
+        this.password = password;
+        this.eMail = eMail;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String geteMail() {
+        return eMail;
+    }
+
+    public Timestamp getAccountCreationTime() {
+        return accountCreationTime;
+    }
 
 }
