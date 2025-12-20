b37928e6288910a3de7f1f1a3a06f8c96c65438d
ceren seydanoğlu <aysecerenseydanoglu@gmail.com>
2025-12-21 00:13:20 +0300

    ui constants and dashboard panel classes are implemented

---
+++ util/UIConstants.java
@@ -0,0 +1,73 @@
+package util;
+import java.awt.Color;
+
+import java.awt.Font;
+
+
+import javax.swing.ImageIcon;
+
+public class UIConstants {
+    
+    public static final Color PRIMARY_GREEN = new Color(120, 190, 32);
+    public static final Color PRIMARY_BLUE;
+    public static final Color PRIMARY_TEAL;
+
+    public static final Color SUCCESS_GREEN;
+    public static final Color DANGER_RED;
+    public static final Color WARNING_ORANGE;
+    public static final Color INFO_BLUE;
+
+    public static final Color BACKGROUND_LIGHT;
+    public static final Color BACKGROUND_DARK;
+    public static final Color TEXT_DARK;
+    public static final Color TEXT_LIGHT;
+    public static final Color BORDER_COLOR;
+
+    public static final Font HEADING_LARGE
+    public static final Font HEADING_MEDIUM;
+    public static final Font HEADING_SMALL;
+    public static final Font BODY_LARGE;
+    public static final Font BODY_MEDIUM;
+    public static final Font BODY_SMALL;
+    public static final Font BUTTON_FONT;
+    public static final Font CAPTION_FONT;
+
+    public static final int WINDOW_WIDHT;
+    public static final int WINDOW_HEIGHT;
+
+    public static final int SIDE_MENU_WIDTH;
+    public static final int CONTENT_WIDTH;
+
+    public static final int BUTTON_HEIGTH;
+    public static final int INPUT_HEIGHT;
+    public static final int CARD_RADIUS;
+    public static final int PRODUCT_CARD_HEIGTH;
+    public static final int PRODUCT_CARD_WIDTH;
+
+    public static final int PADDING_XSMALL;
+    public static final int PADDING_SMALL;
+    public static final int PADDING_MEDIUM;
+    public static final int PADDING_LARGE;
+    public static final int PADDING_XLARGE;
+
+    public static final int MARGIN_SMALL;
+    public static final int MARGIN_MEDIUM;
+    public static final int MARGIN_LARGE;
+
+    public static final String ICON_DASHBOARD;
+    public static final String ICON_BUDGET;
+    public static final String ICON_EXOENSES;
+    public static final String ICON_SHOP;
+    public static final String ICON_CHAT;
+    public static final String ICON_PROFILES;
+    public static final String ICON_SETTINGS;
+    public static final String ICON_LOGOUT;
+
+    public static ImageIcon loadIcon(String path, int width, int height){
+        
+    }
+
+    public static Color getStatusColor(double percentage){
+
+    }
+}
